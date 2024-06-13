c29951cb34f98cf8424ae18c869e592d63bfb765
s.segunshowunmi@gmail.com <s.segunshowunmi@gmail.com>
2024-06-13 13:15:49 -0700

    added lecturer pages

---
+++ src/pages/Lecturer/LecturerHome.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LecturerHome() {
+  return (
+    <div>LecturerHome</div>
+  )
+}
---
+++ src/pages/Lecturer/LecturerMain.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LecturerMain() {
+  return <div>LecturerMain</div>;
+}
