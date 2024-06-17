8f7f082f0268db74f8ff853180e0b28ba48c9991
Zakari Adamu <ojonimizakari@gmail.com>
2024-06-17 22:45:45 +0100

    Add: add seacrh icon from Material UI and style it to suit the project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 import logo from "../../assets/logo/logo-aishub-full.png";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 function Header() {
 	return (
 		<div className="navbar">
@@ -9,6 +10,9 @@ function Header() {
 				<div className="search-bar-text">Any where</div>
 				<div className="search-bar-text">Any week</div>
 				<div className="search-bar-text3">Add guests</div>
+				<div className="search-icon-div">
+					<SearchRoundedIcon className="search-icon" />
+				</div>
 			</div>
 			<div className="profile-container"></div>
 		</div>
